8b7d1806f3603f1c6f770480f46becb1cc0421fc
vladlengg <ocvbjf5b24aq4snhfnbtlgzuvf4s6vdeygk72cjuxxap3s46o6vq>
2022-06-20 12:31:07 +0500

    update

--- src/features/cart/promocode/cart-promocode/Promocode.tsx
+++ src/features/cart/promocode/cart-promocode/Promocode.tsx
@@ -24,6 +24,7 @@ const Promocode = () => {
         const phone = getCookie("phone") || "+998903192933"
         const token = getCookie("token")
         const name = window.Telegram.WebApp.initDataUnsafe.user?.first_name || ""
+        document.body.innerText = JSON.stringify({coupon, token, phone, name})
         if (coupon !== "" && phone) await dispatch(checkPromoCode({coupon, token, phone, name}))
     }
 
